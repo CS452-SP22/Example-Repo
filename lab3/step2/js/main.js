005be3f76e6fcabd54c9ecd81a4c223238b508fa
James Hammer <branson2015@gmail.com>
2022-02-01 17:43:09 -0500

    works from hydra

--- lab3/step2/js/main.js
+++ lab3/step2/js/main.js
@@ -98,9 +98,9 @@ function setupUI() {
 
 // Async as it loads resources over the network.
 async function setupScene() {
-  let objData = await loadNetworkResourceAsText('/shared/resources/models/sphere.obj');
-  let vertSource = await loadNetworkResourceAsText('/shared/resources/shaders/verts/bary300.vert');
-  let fragSource = await loadNetworkResourceAsText('/shared/resources/shaders/frags/bary300.frag');
+  let objData = await loadNetworkResourceAsText('../../shared/resources/models/sphere.obj');
+  let vertSource = await loadNetworkResourceAsText('../../shared/resources/shaders/verts/bary300.vert');
+  let fragSource = await loadNetworkResourceAsText('../../shared/resources/shaders/frags/bary300.frag');
   initializeMyObject(vertSource, fragSource, objData);
 }
 
